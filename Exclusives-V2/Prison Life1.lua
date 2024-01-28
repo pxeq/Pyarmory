83e3cb7dc4fb9b4c7d06ad634f0965d14f97e886
brando <128944260+pxeq@users.noreply.github.com>
2024-01-28 03:24:20 -0500

    Update Prison Life1.lua

--- Exclusives-V2/Prison Life1.lua
+++ Exclusives-V2/Prison Life1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/GwnStefano/NexusHub/main/Main", true))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/LiverMods/xRawnder/main/Project'))()
