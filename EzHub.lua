423df5bea901d2cd858150d55541da31cac01fbd
brando <128944260+pxeq@users.noreply.github.com>
2024-01-28 02:55:50 -0500

    Update EzHub.lua

--- EzHub.lua
+++ EzHub.lua
@@ -2294,7 +2294,7 @@ navDebounce = true;
 --[[
 
 _G.DISABLEEXELOG = true;
-loadstring(game:HttpGet("https://raw.githubusercontent.com/pxeq/Pyarmory/master/EzHub11.lua"))();
+loadstring(game:HttpGet("https://raw.githubusercontent.com/pxeq/Pyarmory/master/EzHub.lua"))();
 
 ]]
 
